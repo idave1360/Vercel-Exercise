9ec22c590c819bc1b8441a4a1e4941e8de69a98f
idave1360 <102980889+idave1360@users.noreply.github.com>
2024-04-30 12:03:48 +0900

    firebase index 수정

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,57 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoItem from '@/components/TodoItem';
+// import styles from '@/styles/TodoList.module.css';
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
+// firebase 관련 모듈을 불러옵니다.
+import { db } from '@/firebase';
+import {
+  collection,
+  query,
+  doc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  orderBy,
+  where,
+} from 'firebase/firestore';
+import { get, set } from 'express/lib/response';
+
+// DB의 todos 컬렉션 참조를 만듭니다. 컬렉션 사용시 잘못된 컬렉션 이름 사용을 방지합니다.
+const todoCollection = collection(db, 'todos');
+
 const TodoList = () => {
+  // 상태를 관리하는 useState 훅을 사용하여 할 일 목록과 입력값을 초기화합니다.
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
 
+  useEffect(() => {
+    getTodos();
+  } , []);
+
+  const getTodos = async () => {
+    // Firestore 쿼리를 만듭니다.
+    const q = query(todoCollection);
+
+    // Fires store 에서 할 일 목록을 조회합니다.
+    const results = await getDocs(q);
+    const newTodos = [];
+
+    // 가져온 할 일 목록을 newTodos 배열에 담습니다.
+    results.docs.forEach((doc) => {
+      // id 값을 Firebase 에 저장한 값으로 지정하고, 나머지 데이터를 newtodos 배열에 담습니다.
+      newTodos.push({ id: doc.id, ...doc.data() });
+    });
+
+    setTodos(newTodos);
+  };
+
   // addTodo 함수는 입력값을 이용하여 새로운 할 일을 목록에 추가하는 함수입니다.
-  const addTodo = () => {
+  const addTodo = async() => {
     // 입력값이 비어있는 경우 함수를 종료합니다.
     if (input.trim() === "") return;
     // 기존 할 일 목록에 새로운 할 일을 추가하고, 입력값을 초기화합니다.
@@ -20,8 +61,15 @@ const TodoList = () => {
     //   completed: 완료 여부,
     // }
     // ...todos => {id: 1, text: "할일1", completed: false}, {id: 2, text: "할일2", completed: false}}, ..
-    const newTodo = { id: Date.now(), text: input, completed: false };
-    setTodos([...todos, newTodo]);
+
+    // Firesotre 에 추가한 일을 저장합니다.
+    const docRef = await addDoc(todoCollection, {
+      text: input,
+      completed: false,
+    });
+
+    // id 값을 Firestore 에 저장한 값으로 지정합니다.
+    setTodos([...todos, { id: docRef.id, text: input, completed: false }]);
     setInput("");
   };
 
@@ -29,30 +77,31 @@ const TodoList = () => {
   const toggleTodo = (id) => {
     // 할 일 목록에서 해당 id를 가진 할 일의 완료 상태를 반전시킵니다.
     setTodos(
-      // todos.map((todo) =>
-      //   todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      // )
-      // ...todo => id: 1, text: "할일1", completed: false
       todos.map((todo) => {
-        return todo.id === id ? { ...todo, completed: !todo.completed } : todo;
+        if (todo.id === id) {
+          // Firestore 에서 해당 id를 가진 할 일의 완료 상태를 업데이트합니다.
+          const todoDoc = doc(todoCollection, id)
+          updateDoc(todoDoc, { completed: !todo.completed });
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
       })
-    );
+    )
   };
 
   // deleteTodo 함수는 할 일을 목록에서 삭제하는 함수입니다.
   const deleteTodo = (id) => {
-    // 해당 id를 가진 할 일을 제외한 나머지 목록을 새로운 상태로 저장합니다.
-    // setTodos(todos.filter((todo) => todo.id !== id));
+    // Firestore 에서 해당 id를 가진 할 일을 삭제합니다.
+    const todoDoc = doc(todoCollection, id);
+    deleteDoc(todoDoc);
+
+    // 해당 id를 가진 할 일ㅇ르 제외한 나머지 목록을 새로운 상태로 저장합니다.
     setTodos(
       todos.filter((todo) => {
         return todo.id !== id;
       })
-    );
-  };
-
-  // deleteAllTodos 함수는 모든 할 일을 목록에서 삭제하는 함수입니다.
-  const deleteAllTodos = () => {
-    setTodos([]);
+    )
   };
 
   // 컴포넌트를 렌더링합니다.
@@ -72,12 +121,7 @@ const TodoList = () => {
         Add Todo
       </Button>
   
-      <Button
-        className="w-full p-1 bg-red-500 text-white rounded hover:bg-red-600 active:translate-y-1 transform transition"
-        onClick={deleteAllTodos}
-      >
-        Delete All
-      </Button>
+
       <ul className="list-none p-0">
         {todos.map((todo) => (
           <TodoItem
